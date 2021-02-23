e221f9790bb0e7e62aebf4d4bf6ffea586cb55b3
Pani-wang <tfuji2626@gmail.com>
2021-02-23 20:14:59 +0800

    commit

---
+++ HelloWorld - 複製.java
@@ -0,0 +1,11 @@
+public class HelloWorld{
+	public static void main(String[] args){
+		private int a = 2;
+		private String Style;
+		private String Cartoon;
+
+			System.out.println("Hello World!");
+	}
+}
+
+//我加了餒
